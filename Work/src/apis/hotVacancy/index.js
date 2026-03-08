7a3221c175abadf4bb34981a3dc4543fee1517aa
Ahmed Raza <ahmedraza@solutionsquares.com>
2026-03-08 14:07:51 +0530

    Delete Work directory

--- Work/src/apis/hotVacancy/index.js
+++
@@ -1,8 +0,0 @@
-import apiCall from "../../utility/axiosInterceptor";
-
-export const getHotVacancy = async (payload) => {
-  return await apiCall.post(
-    `jobOpening/hotvacancy?page=${payload?.page}&perPage=${payload?.perPage}`,
-    payload?.filterData
-  );
-};
--- Work/src/apis/jobapplylist/index.js
+++
@@ -1,8 +0,0 @@
-import apiCall from "../../utility/axiosInterceptor";
-
-// NOTE:
-// The axios interceptor already returns `resp.data`, not the full axios response.
-// So this function will directly resolve to the parsed response body.
-export const getJobApplyListAPI = async (jobId) => {
-  return await apiCall.get(`/job/${jobId}/applicants`);
-};
